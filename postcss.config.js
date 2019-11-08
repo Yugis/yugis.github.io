0700d7c4b3d6c3ebd16e0f8ae9cda7e6d076db80
Omar Tarek <omar.tarek.18@hotmail.com>
2019-11-08 18:07:58 +0000

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project 
+  content: [
+    'index.html',
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss
+  ]
+}
